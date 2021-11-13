280a04506182e02d749ebb004d876fce40c29501
Chinonso Caleb <codenaij@gmail.com>
2021-11-13 08:45:30 +0100

    2. About Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import AboutUs from './pages/AboutUs'
 
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Page Component
+import AboutSection from '../components/AboutSection'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  )
+}
+
+export default AboutUs
