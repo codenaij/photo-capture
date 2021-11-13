c5e00691342371449cd1b25986e9728b8499210b
Chinonso Caleb <codenaij@gmail.com>
2021-11-13 08:56:30 +0100

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 // Page Component
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   )
 }
 
